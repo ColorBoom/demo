566defbf7e3a85bf4410afbc09d6a6378a5029ad
ColorBoom <1433608734@qq.com>
2018-08-21 10:52:32 +0800

    commit Test3

---
+++ demogit3/src/main/java/com/example/demogit3/Demogit3Application.java
@@ -0,0 +1,12 @@
+package com.example.demogit3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demogit3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demogit3Application.class, args);
+    }
+}
