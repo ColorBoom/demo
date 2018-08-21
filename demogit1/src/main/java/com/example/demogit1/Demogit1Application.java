0fd3219e14e24f1a78d891a2d7d1591a5efc2350
ColorBoom <1433608734@qq.com>
2018-08-21 11:55:50 +0800

    commit Test4

--- demogit1/src/main/java/com/example/demogit1/Demogit1Application.java
+++ demogit1/src/main/java/com/example/demogit1/Demogit1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demogit1Application {
 
     public static void main(String[] args) {
+        System.out.println("---");
         SpringApplication.run(Demogit1Application.class, args);
     }
 }
